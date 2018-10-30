2060faf8c2a90c894a67bfe0107df2d3242a4815
kidow <wcgo2ling@hanmail.net>
2018-10-30 19:28:13 +0900

    basic client set

---
+++ client/src/components/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Input = () => {
+  return (
+    <form>
+      <input />
+      <input />
+      <button>추가</button>
+    </form>
+  );
+};
+
+export default Input;
---
+++ client/src/containers/TodosContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodosContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default TodosContainer;
