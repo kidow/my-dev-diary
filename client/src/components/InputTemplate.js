d29bbdbaf1beb4ba41b8dd5cd9556a947e6605a6
kidow <wcgo2ling@hanmail.net>
2018-11-07 01:29:32 +0900

    input autoFocus set

--- client/src/components/InputTemplate.js
+++ client/src/components/InputTemplate.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const InputTemplate = ({ value, learned, onChange, onInsert, onKeyPress, onToggle }) => {
+const InputTemplate = ({ value, learned, onChange, onInsert, onKeyDown, onToggle }) => {
   return (
     <div>
-      <input onChange={onChange} value={value} placeholder='할 일을 입력하세요' onKeyPress={onKeyPress}/>
+      <input onChange={onChange} value={value} placeholder='할 일을 입력하세요' onKeyDown={onKeyDown} autoFocus/>
       <div onClick={onToggle}>
         <input type='checkbox' checked={learned} readOnly/> 배운 것
       </div>
