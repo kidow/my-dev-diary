0e79b5ea3982678d2605249a39fdab51e8e0283c
kidow <wcgo2ling@hanmail.net>
2018-11-30 22:39:52 +0900

    cors

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const PORT = process.env.PORT || 8000
 
 const todosRouter = require('./src/routes/todos')
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGO_URI, { dbName: 'mydevdiary', useNewUrlParser:
 })
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/todos', todosRouter)
 
